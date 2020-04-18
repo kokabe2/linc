aa2a711486a66a3f0ad8f9c360b559f33eab0910
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-18 14:12:17 +0900

    Add Request interface

---
+++ v1/request.h
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_REQUEST_H_
+#define V1_REQUEST_H_
+
+typedef struct RequestInterfaceStruct* Request;
+typedef struct RequestInterfaceStruct {
+  void (*Delete)(Request* self);
+  const char* (*GetUri)(Request self);
+  const char* (*GetDomain)(Request self);
+  const char* (*GetApiVersion)(Request self);
+  const char* (*GetMethod)(Request self);
+  const void* (*GetBody)(Request self);
+  const char* (*GetParam)(Request self, const char* key);
+  int (*ToInt)(const char* param);
+} RequestInterfaceStruct;
+
+#endif  // V1_REQUEST_H_
