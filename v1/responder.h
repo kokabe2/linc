0414be0b0ca4ebdda650b7e405097f3378084f5e
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-18 14:12:25 +0900

    Add Responder interface

---
+++ v1/responder.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_RESPONDER_H_
+#define V1_RESPONDER_H_
+
+typedef struct ResponderInterfaceStruct* Responder;
+typedef struct ResponderInterfaceStruct {
+  void (*SetStatusCode)(Responder self, int status_code);
+  void (*SetBody)(Responder self, const void* body);
+  void (*Send)(Responder self);  // The Responder instance will be automatically destroyed.
+} ResponderInterfaceStruct;
+
+#endif  // V1_RESPONDER_H_
