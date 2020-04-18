e9116f24472c1c446c7d7abe228fad3d644fc04c
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-18 13:58:41 +0900

    Add Response interface

---
+++ v1/response.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_RESPONSE_H_
+#define V1_RESPONSE_H_
+
+typedef struct ResponseInterfaceStruct* Response;
+typedef struct ResponseInterfaceStruct {
+  void (*Delete)(Response* self);
+  int (*GetStatusCode)(Response self);
+  const void* (*GetBody)(Response self);
+} ResponseInterfaceStruct;
+
+#endif  // V1_RESPONSE_H_
