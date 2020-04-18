cda5d31107a39c083587517100d3d637ef1351bb
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-18 21:14:29 +0900

    Add Responder::SetBodyDeleter()

--- v1/responder.h
+++ v1/responder.h
@@ -3,10 +3,13 @@
 #ifndef V1_RESPONDER_H_
 #define V1_RESPONDER_H_
 
+#include "bleu/v1/delegate_definition.h"
+
 typedef struct ResponderInterfaceStruct* Responder;
 typedef struct ResponderInterfaceStruct {
   void (*SetStatusCode)(Responder self, int status_code);
   void (*SetBody)(Responder self, const void* body);
+  void (*SetBodyDeleter)(Responder self, DeleteDelegate delegate);
   void (*Send)(Responder self);  // The Responder instance will be automatically destroyed.
 } ResponderInterfaceStruct;
 
