390d083c86a1514fde418b1c997bf71454218512
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 00:27:20 +0900

    Use NameResolutionHelper instead of private functions

--- src/phttp.c
+++ src/phttp.c
@@ -7,36 +7,18 @@
 #include "bleu/v1/heap.h"
 #include "bleu/v1/list.h"
 #include "bleu/v1/path.h"
-#include "default_router.h"
-#include "default_server.h"
 #include "http_method_request.h"
-#include "pdns.h"
 #include "responder/default_responder.h"
 #include "response/default_response.h"
 #include "response/simple_response.h"
 #include "router/router_base.h"
+#include "util/name_resolution_helper.h"
 
 inline static const char* GetPattern(const char* uri) { return &uri[strings->Index(uri, "/")]; }
 
-inline static Server NewServer(const char* domain) {
-  Router r = defaultRouter->New();
-  Server s = defaultServer->New(domain, r);
-  pdns->Register(s);
-  return s;
-}
-
-inline static Server RetrieveOrNewServer(const char* uri) {
-  Path p = path->New(uri);
-  const char* domain = path->Directory(p, 0);
-  Server s = pdns->Retrieve(domain);
-  if (s == NULL) s = NewServer(domain);
-  path->Delete(&p);
-  return s;
-}
-
 static void Handle(const char* uri, PhttpHandlerDelegate handler) {
   const char* pattern = GetPattern(uri);
-  Server s = RetrieveOrNewServer(uri);
+  Server s = nameResolutionHelper->RetrieveOrNewIfNotFound(uri);
   Router r = serverBase->GetRouter(s);
   r->POST(r, pattern, handler);
   r->PUT(r, pattern, handler);
@@ -46,16 +28,8 @@ static void Handle(const char* uri, PhttpHandlerDelegate handler) {
 
 static RuntimeError ListenAndServe(const char* addr, void* reserved) { return NULL; }
 
-inline static Server RetrieveServer(const char* uri) {
-  Path p = path->New(uri);
-  const char* domain = path->Directory(p, 0);
-  Server s = pdns->Retrieve(domain);
-  path->Delete(&p);
-  return s;
-}
-
 inline static Response HttpMethodTemplate(const char* method, const char* uri, const void* body) {
-  Server s = RetrieveServer(uri);
+  Server s = nameResolutionHelper->Retrieve(uri);
   if (s != NULL) {
     Request req = httpMethodRequest->New(method, uri, body);
     Response res = defaultResponse->New();
