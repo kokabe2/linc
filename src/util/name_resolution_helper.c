577b73da254944af18ff615cbc0c97c3b9d4deeb
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 00:26:53 +0900

    Add NameResolutionHelper class

---
+++ src/util/name_resolution_helper.c
@@ -0,0 +1,38 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "name_resolution_helper.h"
+
+#include "bleu/v1/path.h"
+#include "default_router.h"
+#include "default_server.h"
+
+static Server Retrieve(const char* uri) {
+  Path p = path->New(uri);
+  const char* domain = path->Directory(p, 0);
+  Server s = pdns->Retrieve(domain);
+  path->Delete(&p);
+  return s;
+}
+
+inline static Server NewServer(const char* uri) {
+  Router r = defaultRouter->New();
+  Server s = defaultServer->New(uri, r);
+  pdns->Register(s);
+  return s;
+}
+
+static Server RetrieveOrNewIfNotFound(const char* uri) {
+  Path p = path->New(uri);
+  const char* domain = path->Directory(p, 0);
+  Server s = pdns->Retrieve(domain);
+  if (s == NULL) s = NewServer(domain);
+  path->Delete(&p);
+  return s;
+}
+
+static const NameResolutionHelperMethodStruct kTheMethod = {
+    .Retrieve = Retrieve,
+    .RetrieveOrNewIfNotFound = RetrieveOrNewIfNotFound,
+};
+
+const NameResolutionHelperMethod nameResolutionHelper = &kTheMethod;
---
+++ src/util/name_resolution_helper.h
@@ -0,0 +1,16 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef SRC_UTIL_NAME_RESOLUTION_HELPER_H_
+#define SRC_UTIL_NAME_RESOLUTION_HELPER_H_
+
+#include "pdns.h"
+
+typedef struct {
+  Server (*Retrieve)(const char* uri);
+  Server (*RetrieveOrNewIfNotFound)(const char* uri);
+} NameResolutionHelperMethodStruct;
+typedef const NameResolutionHelperMethodStruct* NameResolutionHelperMethod;
+
+extern const NameResolutionHelperMethod nameResolutionHelper;
+
+#endif  // SRC_UTIL_NAME_RESOLUTION_HELPER_H_
