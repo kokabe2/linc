edbc2fa8b08a3d0db8505fa6f48a081e20553e8d
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-19 09:03:57 +0900

    Add Server interface

---
+++ v1/server.h
@@ -0,0 +1,14 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SERVER_H_
+#define V1_SERVER_H_
+
+#include "bleu/v1/runtime_error.h"
+
+typedef struct ServerInterfaceStruct* Server;
+typedef struct ServerInterfaceStruct {
+  void (*Delete)(Server* self);
+  RuntimeError (*ListenAndServe)(Server self);
+} ServerInterfaceStruct;
+
+#endif  // V1_SERVER_H_
