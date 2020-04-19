475bd18ac10eecc759a5d29e7db39d02ca5e9c5f
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-19 09:00:44 +0900

    Add Router interface

---
+++ v1/router.h
@@ -0,0 +1,17 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_ROUTER_H_
+#define V1_ROUTER_H_
+
+#include "handler.h"
+
+typedef struct RouterInterfaceStruct* Router;
+typedef struct RouterInterfaceStruct {
+  void (*Delete)(Router* self);
+  void (*POST)(Router self, const char* uri, PhttpHandlerDelegate handler);
+  void (*PUT)(Router self, const char* uri, PhttpHandlerDelegate handler);
+  void (*GET)(Router self, const char* uri, PhttpHandlerDelegate handler);
+  void (*DELETE)(Router self, const char* uri, PhttpHandlerDelegate handler);
+} RouterInterfaceStruct;
+
+#endif  // V1_ROUTER_H_
